57e821cb729e17d19fce88fe90d09b6d9634da55
giullian1r <antunigiulliano2@gmail.com>
2024-08-27 23:41:31 -0300

    forca.vfinal

--- jogoDaForca/forca.cpp
+++ jogoDaForca/forca.cpp
@@ -1,36 +1,55 @@
 #include<iostream>
 #include<vector>
-#include <cstdlib>
-#include <ctime>
+#include<cstdlib>
+#include<ctime>
+#include<windows.h>
 using namespace std;
 
-char imprimir_forca(vector<char>& vetor, vector<char>& vetorDeErros, int erros){
+//necessario para deixar as palavras coloridas no console
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+                                                                                        
+char imprimir_forca(vector<char>& vetor,vector<char>& vetorDeErros,vector<char>& vetorDeUsadas, int erros){
     system("cls");
     
-    cout << "|-";
+    cout << "|-";    
+    SetConsoleTextAttribute(h,14);
     for(int i = 0; i < erros; i++){
         cout << vetorDeErros[i];
     }
-    cout << endl;
-    cout << "|" ;
-    for(int i= 0; i < vetor.size(); i ++){
+    SetConsoleTextAttribute(h,7);
+    cout << endl << "|";
+    for(int i = 0; i < vetor.size(); i ++){
         cout << vetor[i];
+    }
+    cout << endl;
+    for(int i = 0; i < vetorDeUsadas.size(); i++){
+        cout << vetorDeUsadas[i] << " ";
     }
 }
 
-char seperar_letras(string& palavra, vector<char>& letras){
-    for(int i= 0; i < palavra.size(); i++){
-            letras.push_back(palavra[i]);
+bool verifica_se_existe_no_vetor(vector<char>& vetor, char x){
+    for (int i = 0; i < vetor.size(); i++)
+    {
+        if(x == vetor[i]){
+            return false;
         }
+    }
+    return true;
 }
 
-char iniciar_forca(string& palavra, vector<char>& forca){
+char seperar_letras(vector<char>& letras,string& palavra){
+    for(int i= 0; i < palavra.size(); i++){
+        letras.push_back(palavra[i]);
+    }
+}
+
+char iniciar_forca(vector<char>& forca,string& palavra){
     for(int i= 0; i < palavra.size(); i++){
         forca.push_back('_');
     }
 }
 
-bool verificar_tentativa(vector<char>& letras, vector<char>& forca, char x){
+bool verificar_tentativa(vector<char>& letras,vector<char>& forca,char x){
     bool acertou = false;
     for(int i= 0; i < letras.size(); i++){
         if(x == letras[i]){
@@ -63,54 +82,83 @@ bool verificar_derrota(vector<char>& vErros,int erros){
     return false;
 }
 
-int main(){
-    vector<string> bancoDePalavras({"vampiro","batman","shrek","gato","dragao"});
-    vector<char> vetorDeErros({'o','-',')','-','-','('});
-    vector<char> letras;
-    vector<char> forca;
+bool verificar_ultima_chance(vector<char>& vErros,int erros){
+    if (erros == vErros.size()){
+        cout <<endl << "Ultima chance!";
+    } 
+}
 
-    srand(time(0)); 
-    int numeroAleatorio = rand() % bancoDePalavras.size();
-
-    string palavraAtual = bancoDePalavras[numeroAleatorio];
-    seperar_letras(palavraAtual, letras);
-    iniciar_forca(palavraAtual, forca);
-
-    bool ganhou = false;
-    bool perdeu = false;
-    int erros = 0;
-    while(!ganhou && !perdeu){
-        char tentativa;
-
-        imprimir_forca(forca,vetorDeErros,erros);
-        if (erros == vetorDeErros.size()){
-            cout <<endl << "Ultima chance!";
-        } 
-        cout << endl << "digite uma letra: ";
-        cin >> tentativa;    
-
-        if(!verificar_tentativa(letras,forca,tentativa)){
-            erros = erros + 1;
-        }
-        ganhou = verificar_vitoria(letras,forca);
-        perdeu = verificar_derrota(vetorDeErros, erros);
-
-    }
+bool resultado_da_forca(vector<char>& forca,vector<char>& vErros,vector<char>& letrasUsadas, int erros,bool ganhou, bool perdeu,int pontos){
     if(ganhou){
-        imprimir_forca(forca,vetorDeErros,erros);
+        imprimir_forca(forca,vErros,letrasUsadas,erros);
         cout << endl;
         cout << "Parabens!!! Voce fugiu da forca :)";
     }
     if(perdeu){
-        imprimir_forca(forca,vetorDeErros,erros - 1);
+        imprimir_forca(forca,vErros,letrasUsadas,erros - 1);
         cout << endl;
         cout << "Voce ficou na forca :/";
     }
+}
 
-    cout << endl << "Deseja jogar novamente? s/n : ";
-    char jogarNovamente;
-    cin >> jogarNovamente;
-    if(jogarNovamente == 's'){
-        main();
+int main(){
+    vector<string> bancoDePalavras({"vampiro","batman","shrek","gato","dragao"});
+    vector<char> vetorDeErros({'o','-',')','-','-','('});
+    vector<char> letras;
+    vector<char> letrasUsadas;
+    vector<char> forca;
+
+    //seleciona uma palavra aleatoria do vetor 
+    srand(time(0)); 
+    int numeroAleatorio = rand() % bancoDePalavras.size();
+    string palavraAtual = bancoDePalavras[numeroAleatorio];
+
+    seperar_letras(letras,palavraAtual);
+    iniciar_forca(forca,palavraAtual);
+
+    bool ganhou = false;
+    bool perdeu = false;
+    int erros = 0;
+    int pontos = 0;
+
+    while(!ganhou && !perdeu){
+        char tentativa;
+
+        imprimir_forca(forca,vetorDeErros,letrasUsadas,erros);
+        verificar_ultima_chance(vetorDeErros,erros);
+
+        cout << endl << "digite uma letra: ";
+        SetConsoleTextAttribute(h,13);
+        cout << endl << "seus pontos: " << pontos << endl;
+        SetConsoleTextAttribute(h,7);
+        cin >> tentativa;    
+
+        if(verifica_se_existe_no_vetor(letrasUsadas,tentativa)){
+            letrasUsadas.push_back(tentativa);
+        }
+        if(!verificar_tentativa(letras,forca,tentativa)){
+            erros = erros + 1;
+            pontos = pontos - 2;
+        }
+        else{
+            pontos = pontos + 5;
+        }
+
+        ganhou = verificar_vitoria(letras,forca);
+        perdeu = verificar_derrota(vetorDeErros, erros);
+    }
+    resultado_da_forca(forca,vetorDeErros,letrasUsadas,erros,ganhou,perdeu,pontos);
+    SetConsoleTextAttribute(h,13);
+    cout << endl << "seus pontos finais: " << pontos << endl;
+    SetConsoleTextAttribute(h,7);
+    
+    int jogarNovamente;
+    while((jogarNovamente != 1) && (jogarNovamente !=2)){
+        cout << endl << "Deseja jogar novamente? 1.sim 2.nao : ";
+        cin >> jogarNovamente;
+
+        if(jogarNovamente == 1){
+            main();
+        }
     }
 }
